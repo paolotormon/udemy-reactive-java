5fec2442eaf92c5046624151f720cede4c58ff9c
Paolo Tormon <paolotormon@gmail.com>
2023-09-23 00:46:52 +0800

    13: Add First Mono

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,10 +11,16 @@ public class FluxAndMonoGeneratorService {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.namesFlux()
                 .subscribe(System.out::println);
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(System.out::println);
     }
 
     public Flux<String> namesFlux() {
         return Flux.fromIterable(List.of("alex", "ben", "chloe")); //db or a
         // remote service call
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("adam");
     }
 }
