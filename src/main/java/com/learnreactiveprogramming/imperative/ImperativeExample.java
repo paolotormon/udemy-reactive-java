0efef8495dfc2748f2b6b2c4c834fcaf7783355c
Paolo Tormon <paolotormon@gmail.com>
2023-09-23 00:08:17 +0800

    11: Imperative Style

--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,24 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImperativeExample {
+    public static void main(String[] args) {
+        var namesList = List.of("Alex", "Ben", "Chloe");
+        var newNamesList = namesGreaterThanLength(namesList, 3);
+        System.out.println(newNamesList);
+    }
+
+    private static List<String> namesGreaterThanLength(List<String> namesList
+            , int length) {
+        var newNamesList = new ArrayList<String>();
+        for (var name : namesList) {
+            if (name.length() > length) {
+                newNamesList.add(name);
+            }
+        }
+        return newNamesList;
+
+    }
 }
