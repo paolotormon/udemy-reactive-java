93bbbd74f84352c3710430cedbe7dff9aa5bdfe1
Paolo Tormon <paolotormon@gmail.com>
2023-09-23 12:43:19 +0800

    15: Testing Flux and Mono using Junit5 and reactor test StepVerifier

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,30 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                //                .expectNext("alex", "ben", "chloe")
+                //                .expectNextCount(3)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono() {
+        var nameMono = fluxAndMonoGeneratorService.nameMono();
+        StepVerifier.create(nameMono).expectNext("adam").verifyComplete();
+    }
 }
