311f40dc113b43a2c9e93494fc64df79d03a9dcf
Paolo Tormon <paolotormon@gmail.com>
2023-09-26 21:08:35 +0800

    50.Reactor Execution Model - Switching Threads with publishOn()

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -1,13 +1,39 @@
 package com.learnreactiveprogramming.service;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
 import java.util.List;
 
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
+@Slf4j
 public class FluxAndMonoSchedulersService {
 
     static List<String> namesList = List.of("alex", "ben", "chloe");
     static List<String> namesList1 = List.of("adam", "jill", "jack");
+
+    Flux<String> explore_publishOn() {
+        var namesFlux = Flux.fromIterable(namesList)
+                .publishOn(Schedulers.parallel())
+                .map(this::upperCase)
+                .map(s -> {
+                    log.info("name is: " + s);
+                    return s;
+                })
+                .log();
+        var namesFlux1 = Flux.fromIterable(namesList1)
+                .publishOn(Schedulers.boundedElastic())
+                .map(this::upperCase)
+                .map(s -> {
+                    log.info("name is: " + s);
+                    return s;
+                })
+                .log();
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
 
     private String upperCase(String name) {
         delay(1000);
---
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersServiceTest.java
@@ -0,0 +1,17 @@
+package com.learnreactiveprogramming.service;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+class FluxAndMonoSchedulersServiceTest {
+
+    FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
+
+    @Test
+    void explore_publishOn() {
+        var flux = fluxAndMonoSchedulersService.explore_publishOn();
+        StepVerifier.create(flux)
+                .expectNextCount(6)
+                .verifyComplete();
+    }
+}
