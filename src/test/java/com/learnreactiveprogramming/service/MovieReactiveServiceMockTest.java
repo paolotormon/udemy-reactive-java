276913525371ad7ea554822d8e7b15c7d7ceac5c
Paolo Tormon <paolotormon@gmail.com>
2023-09-26 14:00:16 +0800

    45. Add exception handling unit tests using Mockito

---
+++ src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
@@ -0,0 +1,53 @@
+package com.learnreactiveprogramming.service;
+
+import com.learnreactiveprogramming.exception.MovieException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+
+@ExtendWith(MockitoExtension.class)
+class MovieReactiveServiceMockTest {
+
+    @InjectMocks
+    MovieReactiveService movieReactiveService;
+    @Mock
+    MovieInfoService movieInfoService;
+    @Mock
+    ReviewService reviewService;
+
+    @Test
+    void getAllMovies() {
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenCallRealMethod();
+        var moviesFlux = movieReactiveService.getAllMovies();
+
+        StepVerifier.create(moviesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void getAllMovie_errorScenario() {
+        var errorMessage = "Exception occured";
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+                .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+                .thenThrow(new MovieException(errorMessage));
+
+        var moviesFlux = movieReactiveService.getAllMovies();
+
+        StepVerifier.create(moviesFlux)
+                .expectError(MovieException.class)
+                .verify();
+    }
+
+}
