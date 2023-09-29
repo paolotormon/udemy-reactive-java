b5890b2bf037f36055c2bd6f42d8871016a7a907
Paolo Tormon <paolotormon@gmail.com>
2023-09-29 19:47:52 +0800

    54. Implement BackPressure

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -1,4 +1,47 @@
 package com.learnreactiveprogramming;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+@Slf4j
 public class BackpressureTest {
+
+    @Test
+    void testBackPressure() {
+        var numberRange = Flux.range(1, 10).log();
+        // normal
+        //        numberRange.subscribe(num -> log.info("num is {}", num));
+        numberRange.subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(2);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                log.info("value - {}", value);
+                if (value == 2) {
+                    cancel();
+                }
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                super.hookOnComplete();
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                super.hookOnError(throwable);
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                log.info("inside onCancel");
+            }
+        });
+    }
 }
