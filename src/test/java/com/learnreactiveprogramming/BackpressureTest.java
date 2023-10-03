733f5d6e6e4c892ed6ca844cbabd365e28b683b5
Paolo Tormon <paolotormon@gmail.com>
2023-10-03 22:05:49 +0800

    56. Handling Backpressure using onBackpressureDrop()

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -91,4 +91,54 @@ public class BackpressureTest {
 
         assertTrue(countDownLatch.await(5L, TimeUnit.SECONDS));
     }
+
+    //    Will show requestUnbounded
+    @Test
+    void testBackPressure_drop() throws InterruptedException {
+        var numberRange = Flux.range(1, 100).log();
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        numberRange
+                .onBackpressureDrop(integer -> {
+                    log.info("Dropped items are {}", integer);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(2);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        log.info("hookOnNext - {}", value);
+                        //                        if (value % 2 == 0) {
+                        //                            request(2);
+                        //                        } else if (value >= 50) {
+                        //                            cancel();
+                        //                        }
+                        if (value == 2) {
+                            hookOnCancel();
+                        }
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+                        super.hookOnComplete();
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        super.hookOnError(throwable);
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+                        log.info("inside hookOnCancel");
+                        countDownLatch.countDown();
+                    }
+                });
+
+        assertTrue(countDownLatch.await(5L, TimeUnit.SECONDS));
+    }
 }
