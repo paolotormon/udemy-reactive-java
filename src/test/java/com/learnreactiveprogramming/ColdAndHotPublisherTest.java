ef6a61e5d21a828f31b5a947bea1b7bda2669c16
Paolo Tormon <paolotormon@gmail.com>
2023-10-09 03:23:10 +0800

    64. Hote Streams - ConnectableFlux

--- src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,7 +1,12 @@
 package com.learnreactiveprogramming;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class ColdAndHotPublisherTest {
 
@@ -11,5 +16,21 @@ public class ColdAndHotPublisherTest {
 
         flux.subscribe(integer -> System.out.println("Subscriber 1 : " + integer));
         flux.subscribe(integer -> System.out.println("Subscriber 2 : " + integer));
+    }
+
+    @Test
+    void hotPublisherTest() {
+        var flux = Flux.range(1, 10)
+                .delayElements(Duration.ofSeconds(1));
+
+        ConnectableFlux<Integer> connectableFlux = flux.publish();
+        connectableFlux.connect();
+
+        connectableFlux.subscribe(integer -> System.out.println("Subscriber 1 : " + integer));
+        delay(2000);
+        connectableFlux.subscribe(integer -> System.out.println("Subscriber 2 : " + integer));
+        delay(10000);
+
+
     }
 }
